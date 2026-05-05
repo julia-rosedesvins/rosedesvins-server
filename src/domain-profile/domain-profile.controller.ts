df51e777310bb8e54d5a3a3523cddc18b1c4ab75
mohammad ammad <mohammadammad144@gmail.com>
2026-05-05 15:52:48 +0500

    feat: update booking restriction time validation to include additional options

--- src/domain-profile/domain-profile.controller.ts
+++ src/domain-profile/domain-profile.controller.ts
@@ -711,9 +711,9 @@ export class DomainProfileController {
 
       // Validate booking restriction time if provided
       if (bookingSettings.bookingRestrictionTime && 
-          !['4h', '24h', '48h', '72h', '7d', '10d'].includes(bookingSettings.bookingRestrictionTime)) {
+          !['last_minute', '1h', '2h', '4h', '24h', '48h', '72h', '7d', '10d'].includes(bookingSettings.bookingRestrictionTime)) {
         throw new HttpException(
-          'Invalid booking restriction time. Must be "24h" or "48h"',
+          'Invalid booking restriction time. Must be one of: last_minute, 1h, 2h, 4h, 24h, 48h, 72h, 7d, 10d',
           HttpStatus.BAD_REQUEST
         );
       }
